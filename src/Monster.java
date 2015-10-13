6d8dd8453ce4ff6bdd1d428e657da3c4c37e5c06
gbacic <gbacic@shaw.ca>
2015-10-13 16:20:59 -0700

    Update Monster.java

--- src/Monster.java
+++ src/Monster.java
@@ -1,12 +1,13 @@
 public class Monster extends game {
 
-	public int health;
-	public int attack;
-
+	public int health;  // The initial health of the monster. Will be read from file.
+	public int attack;  // The attack strengh of the monster. Will be read from file.
+	public String type; // Will be the name the monster appears as in the terminal
 
 	public Monster(){
 	this.health = 30; // stat could change 
 	this.Attack = 10; //
+	this.type = "MonsterTemplate"; // Template name for Monster
 }
 
 public Integer AvoidChance;		/* Between 0-10 */
