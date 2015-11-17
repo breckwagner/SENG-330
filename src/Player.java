d04109422e61f88d4aef5eb18cc316e14c2da2ae
mookTungs <mook13168@hotmail.com>
2015-11-17 14:54:07 -0800

    added player class

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,15 @@
-
-
+import java.util.LinkedList;
 
 public class Player {
 	
-	public Player() {
-		
+	public int health;
+	public int attack;
+	public LinkedList<Item> inventory = new LinkedList<Item>();
+	
+	public Player() 
+	{
+		this.health = 100;
+		this.attackDamage = 10;
 	}
+	
 }
