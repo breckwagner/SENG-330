3584053312e862ef4371f4bd6d57fa33f7288876
gbacic <gbacic@shaw.ca>
2015-11-17 15:01:31 -0800

    added commenting

--- src/Game.java
+++ src/Game.java
@@ -1,16 +1,31 @@
-
+/**
+* 	Creates a new player object(class constructor) for the game and while game is currently a placeholder
+*	for the game obect which will run the game. 
+* 	@author Breck Wagner
+*	@author Mook Tungs
+*	@author Zac Broitman
+*	@author Greg Bacic
+* 
+* 
+* 
+*/
 
 public class Game {
     public Player player = new Player();
     public int currentRooms;
     public int maxRooms;
-
+    /**
+    *  Constructor for the game with static variables.
+    */
     public Game() {
     	currentRooms = 0;
     	maxRooms = 15;
 
     }
-
+    /**
+    *   Constructor for Game with set number of end Rooms
+    *   @param endRooms sets the last number of rooms before the game will end, with the player beating the game.
+    */
     public Game(int endRooms){
     	maxRooms = endRooms;
     }
@@ -21,7 +36,10 @@ public class Game {
     }
     */
 
-
+	/**
+ 	*	checks the pleayrs health while the game is running.
+ 	*	@return  boolean denoting wether the player objects health is greater than 0
+ 	*/
     public boolean isRunning() {
     	if(player.health > 0 && currentRooms <= maxRooms){
     		return true;
