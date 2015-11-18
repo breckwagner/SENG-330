779b8ab98d0cd214bb5f6d07882acfad650364ce
mookTungs <mook13168@hotmail.com>
2015-11-17 19:39:40 -0800

    update test cases

--- src/TestCases.java
+++ src/TestCases.java
@@ -1,20 +1,15 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TestCases{
 	Player player = new Player();
-	Room room = new Room();
-	Monster monster = new Monster(room);
+	Room room1 = new Room();
+	List<Room> listRoom = new ArrayList<Room>();
+	Monster monster = new Monster(room1);
 	
-	//Test player's constructor
-	@Test
-	public void testPlayerConstructor(){
-		assertEquals(100, player.health);
-		assertEquals(10, player.attack);
-		assertEquals(0, player.potion);
-	}
-	
-	//Test player's usePotion()
+	//Test Player's usePotion()
 	@Test
 	public void testUsePotion(){
 		player.potion++;
@@ -22,5 +17,27 @@ public class TestCases{
 		assertEquals(120, player.health);
 		assertEquals(0, player.potion);
 	}
-	
+	@Test
+	//Test Player's receiveDamage()
+	public void testPlayerReceiveDamage()
+	{
+		player.receiveDamage(monster);
+		assertEquals(90, player.health);
+	}
+	//Test Monster's receiveDamage()
+	@Test
+	public void testMonsterReceiveDamage(){
+		monster.receiveDamage(player);
+		assertEquals(10, monster.health);
+	}
+	//Test Room's neighbor list
+	@Test
+	public void testRoomNeighbor(){
+		listRoom.add(room1);
+		Room room2 = new Room(listRoom);
+		listRoom.add(room2);
+		Room room3 = new Room(room2.neighboringRooms);
+		listRoom.add(room3);
+		assertEquals(listRoom.size(), room3.neighboringRooms.size());
+	}
 }
