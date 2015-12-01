d9a8f1482e4613cd0f607258183e21df16f5cc89
Richard B. Wagner <breckwagner@gmail.com>
2015-12-01 11:13:28 -0800

    Added Greg's intro text

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -37,7 +37,7 @@ public class Interface {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
 		
-		System.out.println(Utility.GAME_START_ASCII);
+		System.out.println(Utility.GAME_START_ASCII+ "\n" +Utility.GAME_START_DESCRIPTION);
 		int cols = Utility.getCols();
 		
 		for(int i = 0; i < cols; i++) {
